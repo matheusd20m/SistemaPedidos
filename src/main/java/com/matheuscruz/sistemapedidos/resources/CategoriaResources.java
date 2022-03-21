0a046629f575625d037ec2dd5783c12c8099e619
Matheus Cruz <matheusd20m@gmail.com>
2022-03-21 08:54:10 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/matheuscruz/sistemapedidos/resources/CategoriaResources.java
+++ src/main/java/com/matheuscruz/sistemapedidos/resources/CategoriaResources.java
@@ -1,17 +1,29 @@
 package com.matheuscruz.sistemapedidos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matheuscruz.sistemapedidos.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String find() {
+	public List<Categoria> find() {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		return "Rest está Funcionando!";
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 
