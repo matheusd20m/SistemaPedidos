c731a5fea95e4959e5425adf93760211cb6a90f3
Matheus Cruz <matheusd20m@gmail.com>
2022-03-22 20:31:50 -0300

    Criando operação de instanciacao

--- src/main/java/com/matheuscruz/sistemapedidos/SistemaPedidosApplication.java
+++ src/main/java/com/matheuscruz/sistemapedidos/SistemaPedidosApplication.java
@@ -1,13 +1,34 @@
 package com.matheuscruz.sistemapedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matheuscruz.sistemapedidos.domain.Categoria;
+import com.matheuscruz.sistemapedidos.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class SistemaPedidosApplication {
+public class SistemaPedidosApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPedidosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
