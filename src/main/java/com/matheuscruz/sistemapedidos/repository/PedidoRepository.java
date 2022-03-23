892f62b5913978a0cf9246cd4b79e5c13f4c5e73
Matheus Cruz <matheusd20m@gmail.com>
2022-03-23 19:35:16 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/matheuscruz/sistemapedidos/repository/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.matheuscruz.sistemapedidos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheuscruz.sistemapedidos.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
